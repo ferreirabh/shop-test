76ea349ce98a96e84942c807f066ec2ec0afdd83
Bruno Henrique Ferreira <ferreirabrunodev@gmail.com>
2025-02-23 11:38:03 -0300

    Create project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center">
+    <p>Teste do povo do brazil</p>
+  </div>
+</template>
