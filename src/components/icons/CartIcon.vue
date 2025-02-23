9096ef4ca38a6f6e4addcf2841bb24acbd73e7e2
Bruno Henrique Ferreira <ferreirabrunodev@gmail.com>
2025-02-23 20:40:18 -0300

    Create icon components

---
+++ src/components/icons/CartIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="relative">
+    <svg
+      class="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2 9m5-9v9m4-9v9m4-9l2 9"
+      ></path>
+    </svg>
+    <span
+      v-if="cartItemCount"
+      class="absolute top-0 right-0 bg-red-500 text-white rounded-full text-xs w-5 h-5 flex items-center justify-center"
+      >{{ cartItemCount }}</span
+    >
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  cartItemCount: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
---
+++ src/components/icons/TrashIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    class="w-4 h-4"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M3 6h18M9 6V4a2 2 0 012-2h2a2 2 0 012 2v2m4 0v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6h14z"
+    />
+  </svg>
+</template>
